7137b68f5bf6eb760d25fd5af0f020da447fd1b9
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-09-01 12:20:39 +1000

    abit of a mess if u load it but I'm trying ot load from forbes
    Former-commit-id: 596448b4f915b7e61d8bde99b372e75d56ee68b2

--- html/js/api.js
+++ html/js/api.js
@@ -2,6 +2,10 @@
 var apikey = "j0porbqbr4efdh2c";
 //List of the available trove zones
 var zones = ["map", "collection", "list", "people", "book", "article", "music", "picture", "newspaper"];
+
+//Init forbes var
+var forbes;
+
 //Create an empty object, eventually this will prbably be manually fleshed out
 // when we figure out the weightings.
 var ZoneWeight = {};
@@ -22,18 +26,24 @@ for (var z in zones) {
 // details saved rather than getting passed around in functions
 var result;
 
+function urlBuilder(zone, search) {
+    search = search.replace(/ /g, "%20");
+    return "http://api.trove.nla.gov.au/result?key=" + apikey + "&encoding=json&zone=" +
+        zone + "&q=" + search + "&callback=?";
+}
+
 //get gets the search from the zone and appends the response to the div specified.
 //In its current state it gets the relevance (with weighting) and the relevance
 // without weighting.
-function get(div, search) {
+function get(div, zone, search) {
     //TODO: Some shit when we get 500 response
     //took this out of get cause we shouldn't ever need to specify zone
-    zone = "all";
-    var URL = "http://api.trove.nla.gov.au/result?key=" + apikey + "&encoding=json&zone=" +
-        zone + "&q=" + search + "&callback=?";
+    //zone = "all";
+    URL = urlBuilder(zone, search);
+    console.log(URL);
     $.getJSON(URL, function(response) {
         //Note this runs async so will need a loaded=false/true field
-        console.log(URL);
+
         //Could just pass this in, but we want to be storing this shit
         result = response;
         $(div).append("<h2>" + search + "</h2>With weighting;<br>");
@@ -46,7 +56,7 @@ function get(div, search) {
 
 //get wrapper for the lazy
 function genericGet(search) {
-    get("#search", search);
+    get("#search", "all", search);
 }
 
 function relevanceNoWeighting() {
@@ -67,10 +77,27 @@ function relevance() {
     }
     return total;
 }
+
+function getForbes() {
+    URL = urlBuilder("list", "top") + "&include=listItems";
+    console.log(URL);
+    $.getJSON(URL, function(response) {
+        //console.log(JSON.stringify(response.response.zone[0].records.list[0]));
+        for (var item in response.response.zone[0].records.list[0].listItem) {
+            item = response.response.zone[0].records.list[0].listItem[item].people;
+            $("#forbes").append(JSON.stringify(item));
+            $("#forbes").append("</br>");
+        }
+        //forbes = response;
+        //$("#forbes").append(JSON.stringify(response));
+    });
+}
+
 //run when window is loaded
 $(window).load(function() {
     $(help).append("If nothing is coming up, check if it is 'waiting for trove' in the bottom right corner. If it is refresh the page.");
     $(help).append("Also open up the dev console for more dettails.<br><br>");
+    getForbes();
     genericGet("britney spears");
     genericGet("Steve Irwin");
     genericGet("your mom");
