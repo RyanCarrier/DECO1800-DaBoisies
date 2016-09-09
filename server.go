fd45287699431b79c4d92084a1e3896b7635f768
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-09-09 13:52:11 +1000

    updated to be able to get from api/people/id
    Former-commit-id: 1a6b66f3d4bec0dbcb16a1c63e850eafca6a5ba1

--- server.go
+++ server.go
@@ -1,22 +1,58 @@
 package main
 
 import (
-	"log"
 	"net"
 	"net/http"
+	"strconv"
 	"strings"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 type fileHandler struct {
 	root http.FileSystem
 }
+type apiHandler struct{}
 
 func main() {
-	fh := http.Handler(&fileHandler{http.Dir("html")})
-	http.Handle("/", fh)
 
-	log.Println("Listening on http://localhost:8080/")
-	http.ListenAndServe(":8080", nil)
+	log.Info("Listening on http://localhost:8080/")
+	http.ListenAndServe(":8080", setupHandlers())
+}
+
+func setupHandlers() *http.ServeMux {
+	h := http.NewServeMux()
+	fh := http.Handler(&fileHandler{http.Dir("html")})
+
+	//handle files
+	h.HandleFunc("/api/people/", func(w http.ResponseWriter, r *http.Request) {
+		URL := strings.TrimSpace(r.URL.EscapedPath())[1:]
+		if URL[len(URL)-1] == '/' {
+			URL = URL[:len(URL)-2]
+		}
+		split := strings.Split(URL, "/")
+		if len(split) != 3 {
+			sendHelp(w)
+		} else if i, err := strconv.Atoi(split[2]); err == nil {
+			w.Write([]byte(getName(i)))
+		} else {
+			sendHelp(w)
+		}
+	})
+
+	h.Handle("/", fh)
+	return h
+}
+
+func getName(ID int) string {
+	return strconv.Itoa(ID)
+}
+
+func sendHelp(w http.ResponseWriter) {
+	w.Write([]byte(help()))
+}
+func help() string {
+	return "ur bad\noptions;\n/api/people/:peopleid"
 }
 
 func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -26,9 +62,9 @@ func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		r.URL.Path = upath
 	}
 	if net.ParseIP(r.RemoteAddr) == nil {
-		log.Println(upath, " from ", net.ParseIP(r.RemoteAddr))
+		log.Info(upath, " from ", net.ParseIP(r.RemoteAddr))
 	} else {
-		log.Println(upath, " from ", r.Header.Get("X-Forwarded-For"))
+		log.Info(upath, " from ", r.Header.Get("X-Forwarded-For"))
 	}
 	http.ServeFile(w, r, "html"+upath)
 }
