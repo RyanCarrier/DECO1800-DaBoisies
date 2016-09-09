2c32a7a274c2dc899226d7a4eac77953231151d1
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-09-09 14:55:41 +1000

    api for people works

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"strconv"
@@ -26,6 +28,7 @@ func setupHandlers() *http.ServeMux {
 
 	//handle files
 	h.HandleFunc("/api/people/", func(w http.ResponseWriter, r *http.Request) {
+		logReq(r, r.URL.Path)
 		URL := strings.TrimSpace(r.URL.EscapedPath())[1:]
 		if URL[len(URL)-1] == '/' {
 			URL = URL[:len(URL)-2]
@@ -45,7 +48,29 @@ func setupHandlers() *http.ServeMux {
 }
 
 func getName(ID int) string {
-	return strconv.Itoa(ID)
+	URL := "http://www.nla.gov.au/apps/srw/opensearch/peopleaustralia?q=" + strconv.Itoa(ID)
+	resp, err := http.Get(URL)
+	if err != nil {
+		//We don't give a fuck about http status codes
+		return "ERROR GETTING NAME"
+	}
+	return decodeXML(resp.Body)
+}
+func decodeXML(r io.Reader) string {
+	body, err := ioutil.ReadAll(r)
+	if err != nil {
+		//yo fuck using errors
+		log.Error(err)
+		return "ERROR READING XML"
+	}
+	//HAHA DON"T EVEN PARSE IT
+	split := strings.Split(string(body), "title")
+	if len(split) < 7 {
+		log.Error(split)
+		return "ERROR SPLITTING XML"
+	}
+	split = strings.Split(split[5][1:len(split[5])-2], ", ")
+	return split[1] + " " + split[0]
 }
 
 func sendHelp(w http.ResponseWriter) {
@@ -61,10 +86,14 @@ func (f *fileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		upath = "/" + upath
 		r.URL.Path = upath
 	}
+	logReq(r, upath)
+	http.ServeFile(w, r, "html"+upath)
+}
+
+func logReq(r *http.Request, upath string) {
 	if net.ParseIP(r.RemoteAddr) == nil {
 		log.Info(upath, " from ", net.ParseIP(r.RemoteAddr))
 	} else {
 		log.Info(upath, " from ", r.Header.Get("X-Forwarded-For"))
 	}
-	http.ServeFile(w, r, "html"+upath)
 }
