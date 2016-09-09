07838251f16f2ee121ee9db7073f6712e6c8efa0
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-09-09 13:17:25 +1000

    fixed up server compilation, cause we love solaris, and being lazy...
    Former-commit-id: 48d72358638e076309f42551d56b043de629042b

---
+++ buildAll.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+OSS=("darwin" "windows" "solaris" "linux")
+GOARCH="amd64"
+
+for OS in ${OSS[@]};do
+		OUT="$OS""Server"
+		if [ "$OS" == "windows" ];then
+			OUT="$OUT"".exe"
+		fi
+		echo -ne "Building for $OS...\t"
+		env GOOS=$OS go build -o "$OUT"
+		echo "OK!"
+done
+echo -e "Done!\nAll ARCH=amd64"
