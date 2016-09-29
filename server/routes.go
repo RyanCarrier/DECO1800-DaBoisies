82331bb5c4b07d998ffeabcc84f904dfc7705805
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-09-29 12:36:26 +1000

    clean up logging handler, clean up route adding and definition

--- server/routes.go
+++ server/routes.go
@@ -1,1 +1,21 @@
 package server
+
+import "net/http"
+
+//Routes just wraps the Route in a slice for easier readibility
+type Routes []Route
+
+//Route specifies a single route to define
+type Route struct {
+	Path    string
+	Handler func(http.ResponseWriter, *http.Request)
+}
+
+//GetRoutes gets all the set routes
+func GetRoutes() Routes {
+	return Routes{
+		Route{"/log", handleLog},
+		Route{"/api/people/", handleNames},
+		Route{"/api/getlist/", handleList},
+	}
+}
