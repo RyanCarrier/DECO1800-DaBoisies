29568a270a52d7d6b4f3520e7a141b717f597c95
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-09-29 12:16:41 +1000

    added tests and corrected parsing and returning of json

--- server/trove_test.go
+++ server/trove_test.go
@@ -2,50 +2,104 @@ package server
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"strings"
 	"testing"
 )
 
 func TestParseSearchAll(t *testing.T) {
+	got := getTopResponse(t).Clean()
+	want := CleanResponse{
+		Query: "bob ross",
+		Zones: []CleanZone{
+			CleanZone{
+				Name:  "people",
+				Total: 21,
+			},
+			CleanZone{
+				Name:  "book",
+				Total: 2770,
+			},
+			CleanZone{
+				Name:  "picture",
+				Total: 2172,
+			},
+			CleanZone{
+				Name:  "music",
+				Total: 3126,
+			},
+			CleanZone{
+				Name:  "list",
+				Total: 119,
+			},
+			CleanZone{
+				Name:  "map",
+				Total: 104,
+			},
+			CleanZone{
+				Name:  "collection",
+				Total: 567,
+			},
+			CleanZone{
+				Name:  "article",
+				Total: 47880,
+			},
+			CleanZone{
+				Name:  "newspaper",
+				Total: 146843,
+			},
+		},
+	}
+	marshalAndTest(t, got, want)
+
+}
+
+func TestReturnResult(t *testing.T) {
+	got := getTopResponse(t).Clean().Return()
+	want := CleanReturn{
+		Query: "bob ross",
+		//Year:blank
+		Total: 146843 + 47880 + 567 + 104 + 119 + 3126 + 2172 + 2770 + 21,
+	}
+	marshalAndTest(t, got, want)
+}
+func TestReturnResultWithYear(t *testing.T) {
+	gotr := getTopResponse(t).Clean()
+	gotr.Year = 1234
+	got := gotr.Return()
+	want := CleanReturn{
+		Query: "bob ross",
+		Year:  1234,
+		Total: 146843 + 47880 + 567 + 104 + 119 + 3126 + 2172 + 2770 + 21,
+	}
+	marshalAndTest(t, got, want)
+}
+
+func getTopResponse(t *testing.T) TopResponse {
 	body, err := ioutil.ReadFile("testsearch.json")
 	if err != nil {
 		t.Error("Error reading from test file", err)
 	}
-	var got TopResponse
-	err = json.Unmarshal(body, &got)
+	var gotr TopResponse
+	err = json.Unmarshal(body, &gotr)
 	if err != nil {
 		t.Error("Error unmarshaling", err)
 	}
-	want := TopResponse{
-		response: []Response{
-			Response{
-				query: "test",
-				zone: []Zone{
-					Zone{
-						name: "test",
-						records: Record{
-							total: 0,
-						},
-					},
-				},
-			},
-		},
-	}
+	return gotr
+}
 
-	gots, err := json.Marshal(got)
+func marshalAndTest(t *testing.T, got, want interface{}) {
+	gotb, err := json.Marshal(got)
 	if err != nil {
 		t.Error("Error marshaling got", err)
 	}
-	wants, err := json.Marshal(want)
+	wantb, err := json.Marshal(want)
 	if err != nil {
 		t.Error("Error marshaling want", err)
 	}
-	fmt.Println(string(gots))
-	fmt.Println(string(wants))
-	if strings.Compare(string(gots), string(wants)) != 0 {
-		t.Error("GOT\n", gots, "WANT\n", wants)
+	gots := string(gotb)
+	wants := string(wantb)
+	if strings.Compare(gots, wants) != 0 {
+		t.Error("GOT\n", gots, "\nWANT\n", wants)
 	}
-	fmt.Println(string(gots))
 }
