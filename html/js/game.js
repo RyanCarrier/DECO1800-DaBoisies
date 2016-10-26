d5b50d8a41804d06080993e35e0844a761c02dfd
Ryan Carrier <ryan.j.carrier@gmail.com>
2016-10-26 13:28:49 +1000

    formatting

--- html/js/game.js
+++ html/js/game.js
@@ -4,7 +4,7 @@ var zones = ["map", "collection", "list", "people", "book", "article", "music", 
 var names = [];
 var people;
 var sqbox = [0, 0, 0, 0, 0, 0];
-var lastClicked; 
+var lastClicked;
 
 
 $(window).load(function() {
@@ -316,6 +316,7 @@ closeStep1Help.onclick = function() {
 };
 
 function toStep2() {
+
     document.getElementById("s2Heading").innerHTML = "<h1>Step 2: Muzz It Out</h1>";
     document.getElementById("cont").onclick = function() {
         toStep3();
@@ -452,17 +453,17 @@ function allowDrop(ev) {
 
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-	var id = "#"+ev.target.id; 
-	var parentId = $(id).parent().attr("id")
-	lastClicked = parentId; 
+    var id = "#" + ev.target.id;
+    var parentId = $(id).parent().attr("id")
+    lastClicked = parentId;
 }
 
 function dropDefault(ev) {
     //$("#cards").append(ev.);
-	//ev.target.append(document.getElementById("cards"));
-	var data = ev.dataTransfer.getData("text");
+    //ev.target.append(document.getElementById("cards"));
+    var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-	
+
 }
 
 function drop(ev) {
@@ -470,26 +471,26 @@ function drop(ev) {
     var target = $(event.target);
     console.log(target.attr('id'));
     var id = target.attr("id");
-	var i = +id[2] - 1;
+    var i = +id[2] - 1;
     if (id.length == 3 && id[0] == 's' && id[1] == 'm') {
-		if (sqbox[i] === 0) {
+        if (sqbox[i] === 0) {
             dropDefault(ev);
             sqbox[i] = 1;
-			id = lastClicked;
-			a = +id[2] - 1;
-			sqbox[a] = 0;
+            id = lastClicked;
+            a = +id[2] - 1;
+            sqbox[a] = 0;
         } else {
             console.log("nup");
         }
     } else if (id === "cards") {
-		dropDefault(ev);
-		id = lastClicked;
-		i = +id[2] - 1;
-		sqbox[i] = 0;
-		console.log(i);
+        dropDefault(ev);
+        id = lastClicked;
+        i = +id[2] - 1;
+        sqbox[i] = 0;
+        console.log(i);
     } else if (id[0] == 'c') {
         console.log("don't put me on another card u retard");
     } else {
-		dropDefault(ev);
+        dropDefault(ev);
     }
 }
